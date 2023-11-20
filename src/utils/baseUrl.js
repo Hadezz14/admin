4b8a3de80c54e2f01486e8933d2de6b4943e89b7
root <root@ubuntu-cloudpanel.localhost>
2023-11-20 02:21:25 +0000

    vpsBuild

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,5 @@
 // export const base_url = "https://vyam-backend.onrender.com/api/";
 
-// export const base_url = "https://api.vyamstore.com/api/";
-export const base_url = "http://localhost:5069/api/";
+
+export const base_url = "https://api.vyamstore.com/api/";
+>>>>>>> 4f86645 (vpsBuild)
